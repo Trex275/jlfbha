ed4b7f888830cd4c921c1a03b219f050cd368962
Trex275 <73885729+Trex275@users.noreply.github.com>
2021-03-20 13:27:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+//Create variables here
+var dog;
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(500, 500);
+  dog.createSprite
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
